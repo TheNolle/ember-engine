5bbea202275bbbbb0e0c43e61407a8dae56f121c
TheNolle <github@thenolle.com>
2025-05-12 19:06:06 +0200

    feat(physics) 🧱 add AABB collision detection and grounded floor logic for player jumping and landing
    Signed-off-by: TheNolle <github@thenolle.com>

--- src/playground/objects/Player.ts
+++ src/playground/objects/Player.ts
@@ -3,6 +3,7 @@ import { PhysicsComponent } from '@core/physics/PhysicsComponent'
 
 export class Player extends GameObject {
 	physics = new PhysicsComponent({ gravity: 600, friction: 0.95 })
+	isGrounded = false
 
 	constructor() {
 		super({ width: 48, height: 48 })
--- src/playground/scenes/TestScene.ts
+++ src/playground/scenes/TestScene.ts
@@ -1,3 +1,5 @@
+import { aabb } from '@core/physics/collisions'
+import { GameObject } from '@core/game/GameObject'
 import { InputManager } from '@core/input/InputManager'
 import { Scene } from '@core/scene/Scene'
 import { Canvas2DRenderer } from '@core/renderer/Canvas2DRenderer'
@@ -7,12 +9,23 @@ import { Player } from '../objects/Player'
 export class TestScene extends Scene {
 	private renderer = new Canvas2DRenderer()
 	private input = new InputManager()
+
 	private player = new Player()
 	private playerShape = new Rect(this.player.x, this.player.y, this.player.width, this.player.height, 'cyan')
+
+	private floor = new class extends GameObject {
+		constructor() {
+			super({ id: 'floor', x: 0, y: 600, width: 1280, height: 40 })
+			this.addTag('solid')
+		}
+	}
+	private floorShape = new Rect(this.floor.x, this.floor.y, this.floor.width, this.floor.height, 'gray')
 
 	init(): void {
 		const canvas = document.querySelector('canvas')!
 		this.renderer.init(canvas)
+
+		this.renderer.add(this.floorShape, 0)
 		this.renderer.add(this.playerShape)
 
 		this.input.bind('left', 'ArrowLeft')
@@ -27,10 +40,20 @@ export class TestScene extends Scene {
 		const speed = 400
 		if (this.input.isDown('right')) this.player.physics.vx += speed * dt
 		if (this.input.isDown('left')) this.player.physics.vx -= speed * dt
-		if (this.input.isDown('up')) this.player.physics.vy -= speed * dt
-		if (this.input.isDown('down')) this.player.physics.vy += speed * dt
+		if (this.input.isDown('up') && this.player.isGrounded) {
+			this.player.physics.vy = -400
+		}
 
 		this.player.physics.apply(dt, this.player)
+
+		// Ground collision
+		if (aabb(this.player, this.floor)) {
+			this.player.y = this.floor.y - this.player.height
+			this.player.physics.vy = 0
+			this.player.isGrounded = true
+		} else {
+			this.player.isGrounded = false
+		}
 
 		this.playerShape.x = this.player.x
 		this.playerShape.y = this.player.y
